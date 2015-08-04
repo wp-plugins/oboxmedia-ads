937349d176701170ee914001368844f208cbf359
patforg <patforg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 13:56:46 +0000

    bumping up version
    git-svn-id: https://plugins.svn.wordpress.org/oboxmedia-ads/trunk@1212618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- OboxmediaAdsPlugin.php
+++ OboxmediaAdsPlugin.php
@@ -23,7 +23,7 @@ class OboxmediaAdsPlugin{
      *
      * @var     string
      */
-    protected $version = "1.0.0";
+    protected $version = "1.0.1";
 
     /**
      * Unique identifier for your plugin.
